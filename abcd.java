c73ca159c03cc3424566db730a4e777fcf71f5e0
Trevor Holland <trevorholland@outlook.com>
2014-10-27 18:23:07 -0400

    Created new Java file

---
+++ abcd.java
@@ -0,0 +1,13 @@
+public class Default 
+{
+    public static void main(String[] args)
+  {
+      int a,b,c;
+      a=0;
+      b=1;
+      c=2;
+      
+      System.out.print(a + b + c);
+  }
+
+}
